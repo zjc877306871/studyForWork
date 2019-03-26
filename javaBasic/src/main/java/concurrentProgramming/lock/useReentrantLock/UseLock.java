a250e1d5ceceb2871e2875b192800b8432750716
zhangjc <1204880901@qq.com>
2019-03-26 16:31:30 +0800

    锁

---
+++ javaBasic/src/main/java/concurrentProgramming/lock/useReentrantLock/UseLock.java
@@ -0,0 +1,25 @@
+package concurrentProgramming.lock.useReentrantLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by user on 2018/9/6.
+ */
+public class UseLock {
+
+    public static class BusinessThread implements Runnable{
+        Lock lock = new ReentrantLock();
+        private int count;
+        @Override
+        public void run() {
+            lock.lock();
+            try{
+                count++;
+                System.out.println("枷锁来啦"+count);
+            }finally {
+                lock.unlock();
+            }
+        }
+    }
+}
