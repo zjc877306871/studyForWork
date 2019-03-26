05099d651ccd28ac76794335859fff6a3182a54e
zhangjianchong <zhangjianchong@rmitec.com>
2019-03-26 22:41:36 +0800

    cyclicBrries

--- javaBasic/src/main/java/com/late/concurrentProgramming/concurrentTools/CycilcBrries/TomCyclicBrries.java
+++ javaBasic/src/main/java/com/late/concurrentProgramming/concurrentTools/CycilcBrries/TomCyclicBrries.java
@@ -1,8 +1,10 @@
-package com.late.concurrentProgramming.concurrentTools.CycilcBrries;/**
+package com.zhangjc.javaBasic.myThread.synchronizationTools.CycilcBrries;/**
  * Created by user on 2019/3/22.
  */
 
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CyclicBarrier;
 
@@ -22,12 +24,50 @@ public class TomCyclicBrries {
      */
     private static class CountThread implements Runnable{
 
+        StringBuffer buffer = new StringBuffer();
         @Override
         public void run() {
             for(Map.Entry<String, Long> entry : hashMap.entrySet()){
 
+                buffer.append(entry.getValue());
+
             }
+            System.out.println("共执行了："+buffer.toString()+"个任务");
         }
     }
 
+
+    private static class BussinessThread implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("当前工作线程名字：" + Thread.currentThread().getName() + "进行一些公共的业务");
+            hashMap.putIfAbsent(Thread.currentThread().getId() + "",Thread.currentThread().getId());
+            Random random = new Random();
+            if(random.nextBoolean()){
+                System.out.println("当前工作线程:" + Thread.currentThread().getId() + "执行111操作");
+            }else{
+                System.out.println("当前工作线程:" + Thread.currentThread().getId() + "执行222操作");
+            }
+
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("当前工作线程名字：" + Thread.currentThread().getId() + "进行业务操作");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<3; i++){
+            new Thread(new BussinessThread()).start();
+        }
+
+    }
 }
